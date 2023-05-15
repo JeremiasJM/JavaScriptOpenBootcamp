f96e3d3aebc7e3b9bbe935e311cea66ab00241a8
Jeremias Juarez Marrades <jeremiasjuarezmarrades15@hotmail.com>
2023-05-15 14:57:43 -0300

    listado

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo".')
+
+const nombre = "Jeremias";
+
+const edad = 24;
+
+
+const EresDesarrollador = true;
+
+
+const fechaNacimiento = new Date(1998, 11, 14);
+
+
+const libroFavorito = {
+  titulo: "La Biblia",
+  autor: "Varios",
+  fecha: new Date(0000, 0, 00),
+  url: "https://ejemplo.com/libro"
+};
